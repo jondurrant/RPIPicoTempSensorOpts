729d3e9b4532868179a901673218ff360dee82e4
Jon Durrant <jon@jondurrant.com>
2023-12-04 14:25:17 +0000

    Added internal sensor.

--- src/main.cpp
+++ src/main.cpp
@@ -16,11 +16,28 @@
 #include <stdio.h>
 #define DELAY 1000
 
+#include "hardware/adc.h"
+
+
+
+float getTemp(){
+	const float conversion_factor = 3.3f / (1 << 12);
+	adc_select_input(4);
+
+	float v = (float)adc_read() * conversion_factor;
+	float t = 27.0 - ((v - 0.706)/0.001721);
+
+	return t;
+}
+
 
 
 int main() {
 	int i = 0;
 	i2c_inst_t * i2c =  & i2c0_inst;
+
+	adc_init();
+	adc_set_temp_sensor_enabled(true);
 
 	DS3231 rtc( i2c, 20, 21);
 	LIB_AHTXX myAHT10(AHT10_ADDRESS_0X38, i2c0, 20,  21, 100);
@@ -40,15 +57,16 @@ int main() {
 	}
 
 	for(;;) {
-
-		printf("Count %d\n", i++);
+		printf("\n");
+		printf("Internal: %fC\n", getTemp());
+		printf("DS18B20 %fC\n", ds.getTemperature());
+				ds.convert();
 		printf("DS3231: %s -- %s -- %d%C %.2fC\n", rtc.get_date_str(), rtc.get_time_str(), rtc.get_temp(), rtc.get_temp_f());
 		printf("AHT10 Temp %fC Humidity %f\n",
 				myAHT10.AHT10_readTemperature(true),
 				myAHT10.AHT10_readHumidity(true));
 
-		printf("DS18B20 %fC\n", ds.getTemperature());
-		ds.convert();
+
 		sleep_ms(DELAY);
 
 
